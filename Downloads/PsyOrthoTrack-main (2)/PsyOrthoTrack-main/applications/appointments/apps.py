85e835ec1c97ca4511b658a6fca6dfc852a59503
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2025-04-03 17:45:50 +0100

    Delete PsyOrthoTrack-main directory

--- Downloads/PsyOrthoTrack-main (2)/PsyOrthoTrack-main/applications/appointments/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppointmentsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "applications.appointments"
-    verbose_name = "Rendez-vous" 
--- Downloads/PsyOrthoTrack-main (2)/PsyOrthoTrack-main/applications/patients_docs/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PatientsDocsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "applications.patients_docs"
-    verbose_name = "" 
